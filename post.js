c2a75a82263c3903705145fb063b34529da238d7
Iqra khan <ik22082002khan@gmail.com>
2023-07-31 16:35:21 +0000

    exersise post program

---
+++ post.js
@@ -0,0 +1,11 @@
+const post = {
+    title : " a ",
+    body : "b",
+    Auther : "c",
+    view : 10,
+    comeents : [ 
+        { authore : "a " , body: " b " },
+{     authore : " b " , body : " f "}],
+ }
+
+console.log(post);
